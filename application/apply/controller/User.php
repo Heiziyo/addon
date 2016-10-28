6532e55f495b35b771393e555cede104e7ea6428
gsygsy <2269563911@qq.com>
2016-10-28 09:37:29 +0800

    update:code

--- application/apply/controller/User.php
+++ application/apply/controller/User.php
@@ -7,19 +7,53 @@
  */
 
 namespace app\apply\controller;
+
 use app\common\controller\Base;
+use app\common\helper\VerifyHelper;
 
-class User extends Base{
-
-
-        public function index(){
-
+class User extends Base
+{
 
 
+    public function register()
+    {
+        
+        return $this->view->fetch();
+    }
 
+    public function login()
+    {
 
+        return $this->view->fetch();
+    }
+
+    public function forget()
+    {
+
+        return $this->view->fetch();
+    }
+
+    /**
+     * 显示验证码图片
+     */
+    public function verify()
+    {
+        VerifyHelper::verify();
+    }
+
+    /**
+     * 检测验证码是否正确
+     */
+    public function check()
+    {
+        $code = isset($_POST['code']) ? trim($_POST['code']) : '';
+        $check = VerifyHelper::check($code);
+        if($check) {
+            echo '验证码正确';
+        } else {
+            echo '验证码错误';
         }
-
+    }
 
 
 }
