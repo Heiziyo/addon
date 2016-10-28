e97d44a4c0c90530109e3bcc3b711f486662235f
gsygsy <2269563911@qq.com>
2016-10-28 18:11:07 +0800

    update: register

--- application/apply/controller/User.php
+++ application/apply/controller/User.php
@@ -8,6 +8,7 @@
 
 namespace app\apply\controller;
 
+use app\apply\model\UserModel;
 use app\common\controller\Base;
 use app\common\helper\VerifyHelper;
 
@@ -17,19 +18,16 @@ class User extends Base
 
     public function register()
     {
-        
         return $this->view->fetch();
     }
 
     public function login()
     {
-
         return $this->view->fetch();
     }
 
     public function forget()
     {
-
         return $this->view->fetch();
     }
 
@@ -42,17 +40,33 @@ class User extends Base
     }
 
     /**
-     * 检测验证码是否正确
+     * 注册页逻辑
      */
-    public function check()
+    public function doRegister()
     {
         $code = isset($_POST['code']) ? trim($_POST['code']) : '';
-        $check = VerifyHelper::check($code);
-        if($check) {
-            echo '验证码正确';
-        } else {
-            echo '验证码错误';
+        $post['fname'] = isset($_POST['name']) ? stripslashes(trim($_POST['name'])) : '';
+        $post['femail'] = isset($_POST['email']) ? trim($_POST['email']) : '';
+        $post['fpassword'] = isset($_POST['password']) ? md5(trim($_POST['password'])) : '';
+        //注册时间
+        $post['fregtime'] = time();
+        //创建激活码
+        $post['ftoken'] = md5($post['fname'].$post['fpassword'].$post['fregtime']);
+        //激活码过期时间限制
+        $post['ftokenexptime'] = time() + 3600*24;
+        $user_model = new UserModel();
+        //验证用户名
+        $result = $user_model->userName($post['fname']);
+        if ($result) {
+            $this->error('该用户名已被注册，换一个吧~');
         }
+        //验证验证码
+        $check = VerifyHelper::check($code);
+        if(!$check) {
+            $this->error('验证码错误');
+        }
+        //验证验证码
+
     }
 
 
